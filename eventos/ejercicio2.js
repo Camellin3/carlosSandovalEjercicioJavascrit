f6a77f5d79aec1b633fc28c32ab1a623251ccf49
Camellin3 <carlitos1810052@gmail.com>
2024-07-17 02:48:38 -0500

    Subiendo ya corregido

--- eventos/ejercicio2.js
+++ eventos/ejercicio2.js
@@ -2,9 +2,9 @@ let dolar = document.getElementById("dolar-input")
 let peso = document.getElementById("peso-input")
 
 dolar.addEventListener('keyup', a=>{
-     peso.value = a.target.value * 3975.62
+     peso.value = a.target.value * 3975,62
 })
 
 peso.addEventListener('keyup', a=>{
-    dolar.value = a.target.value / 3975.62
+    dolar.value = a.target.value / 3975,62
 })
--- eventos/ejercicio3.js
+++ eventos/ejercicio3.js
@@ -1,96 +1,101 @@
-let notas=[
-    {
-    id:1,
+let notas = [
+  {
+    id: 1,
     titulo: 'Sacar la basura',
     texto: 'mi mama me va a retar si no lo hago',
     realizada: false
-    },
-    {
-        id:2,
-        titulo: 'Lavar los platos',
-        texto: 'mi mama me va a retar si no lo hago',
-        realizada: false
-    },
-    {
-        id:3,
-        titulo: 'hacer la tarea de programación',
-        texto: 'debe hacerlo para aprobar la materia',
-        realizada: false
-    },
-    {
-        id:4,
-        titulo: 'hacer la tarea de matematicas',
-        texto: 'debe hacerlo para aprobar la materia',
-        realizada: false
-        }
+  },
+  {
+    id: 2,
+    titulo: 'Comer',
+    texto: 'Quedo comida de ayer',
+    realizada: false
+  },
+  {
+    id: 3,
+    titulo: 'Estudiar eventos',
+    texto: 'Estoy flojo de papeles y no voy a aprovar la task 3',
+    realizada: false
+  },
+  {
+    id: 4,
+    titulo: 'Tomar agua',
+    texto: 'Debo hidratarme bien para no desmayarme',
+    realizada: false
+  }
 ];
 
-let idGlobal = 4
+let idGlobal = 5;
 
-function pintarTarjetas(notas){
-    let contenedor = document.getElementById("contenedortarjetas")
-contenedor.innerHTML ="";
-if(notas.length===0){
-  contenedor.innerHTML = "NO HAY NOTAS PARA MOSTRAR";
-}else{
-  notas.forEach(nota =>{
+function pintarTarjetas(notas) {
+  let contenedor = document.getElementById("contenedortarjetas")
+  contenedor.innerHTML = "";
+  if (notas.length === 0) {
+    contenedor.innerHTML = "NO HAY NOTAS PARA MOSTRAR";
+  } else {
+    notas.forEach(nota => { 
 
-    let tarjeta = document.createElement('div');
-    tarjeta.className= "card";
-    
+      let tarjeta = document.createElement('div');
+      tarjeta.className = "card";
+
       tarjeta.innerHTML = `
-        <div class="card col">     
+  <div class="card col">     
     <div class="card-body">
-        <h5 class="card-title">${nota.titulo}</h5>
-        <p class="card-text">${nota.texto}</p>
+      <h5 class="card-title ${nota.realizada ? 'text-decoration-line-through' : ''}">${nota.titulo}</h5>
+      <p class="card-text ${nota.realizada ? 'text-decoration-line-through' : ''}">${nota.texto}</p>
     </div>
     <div class="card-footer d-flex justify-content-between">
-        <span>${nota.realizada}</span>
-      
-        <button onClick="borrarNota(${nota.id})" class="btn btn-primary" id="borrar-nota">Borrar nota
-       </button>
-
-       <input onClick="marcarRealizada(${nota.id})" type="checkbox" ${nota.realizada? 
-"checked": ""}>
-    </div>
+     
     
-    </div>
-      `
+      <button onClick="borrarNota(${nota.id})" class="btn btn-primary" id="borrar-nota">Borrar nota
+     </button>
 
-      contenedor.appendChild(tarjeta); 
+     <input onClick="marcarRealizada(${nota.id})" type="checkbox" ${nota.realizada ? "checked" : ""}>
+
+    </div>
+  
+  </div>
+    `
+
+      contenedor.appendChild(tarjeta);
     });
-      
+
   }
 
 }
-   
-function agregarNota (titulo, texto){
- let nota = {
-  id: idGlobal,
-  titulo: titulo,
-  texto: texto,
-  realizada: false
- };
- idGlobal ++;
- notas.push(nota);
- pintarTarjetas(notas);
+
+pintarTarjetas(notas.slice(0, 4));
+
+function agregarNota(titulo, texto) {
+  let nota = {
+    id: idGlobal,
+    titulo: titulo,
+    texto: texto,
+    realizada: false
+  };
+  idGlobal++;
+  notas.push(nota);
+  pintarTarjetas(notas);
 }
 
 let botonguardar = document.getElementById("boton-guardar")
 
- botonguardar.addEventListener("click", a=>{
+botonguardar.addEventListener("click", a => {
   let titulo = document.getElementById("titulo").value;
   let texto = document.getElementById("texto").value;
-  if(titulo !== "" && texto !== ""){
+  if (titulo !== "" && texto !== "") {
     agregarNota(titulo, texto);
-    document.getElementById("titulo").value ="";  
-    
+    document.getElementById("titulo").value = "";
+    document.getElementById('texto').value = '';
   
-    }
-  })
+  }
+  else if (titulo === "" || texto === "") {
+    alert("Debe completar todos los campos");
+  } 
+})
 
 function borrarNota(id) {
-  notas = notas.filter(nota => nota.id!== id);
+  notas = notas.filter(nota => nota.id !== id);
   pintarTarjetas(notas);
 }
 
@@ -99,19 +104,17 @@ document.getElementById('boton-borrar').addEventListener('click', function () {
   document.getElementById('texto').value = '';
 });
 
-function marcarRealizada(id){
+function marcarRealizada(id) {
   let nota = notas.find(nota => nota.id === id);
-  nota.realizada =! nota.realizada;
+  nota.realizada = !nota.realizada;
+
   pintarTarjetas(notas);
 }
 
-function filtrarPorRealizadas(notas){
-  return notas.filter(nota => nota.realizada);
-}
+function filtrarPorRealizadas(notas) {
 
-function filtrarPorTexto(notas, texto){
-  if(!texto) return notas;
-  return notas.filter(nota => nota.titulo.includes(texto)|| nota.texto.includes(texto));
+  return notas.filter(nota => nota.realizada);
+
 }
 
 const filtroRealizadas = document.getElementById('filtro-realizadas');
@@ -127,7 +130,7 @@ function filtrarPorRealizadas(notas) {
 
 const filtroTexto = document.getElementById('filtro-texto');
 
-filtroTexto.addEventListener('input', () => {
+filtroTexto.addEventListener('keyup', () => {
   const textoFiltro = filtroTexto.value.toLowerCase();
   const notasFiltradas = filtrarPorTexto(notas, textoFiltro);
   pintarTarjetas(notasFiltradas);
